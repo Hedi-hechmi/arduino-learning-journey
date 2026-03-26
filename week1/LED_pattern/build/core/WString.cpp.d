f815240ef99f0a406dfe31985b8601fb368cc2ee
Hedi Hechmi <hedihechmi6@gmail.com>
2026-03-26 10:31:46 +0100

    Clean and organize LED_pattern project structure

--- week1/LED_pattern/build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Hedi\arduino-learning-journey\week1\LED_\LED_pattern\build\core\WString.cpp.o: \
- C:\Users\Hedi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\WString.cpp \
- C:\Users\Hedi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\WString.h
--- week1/LED_pattern/build/core/new.cpp.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Hedi\arduino-learning-journey\week1\LED_\LED_pattern\build\core\new.cpp.o: \
- C:\Users\Hedi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new.cpp \
- C:\Users\Hedi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new.h \
- C:\Users\Hedi\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.7\cores\arduino\new
